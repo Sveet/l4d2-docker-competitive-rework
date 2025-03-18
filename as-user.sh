519d543daca8c8f7d1972319b3a3f1e2687ef3d6
Joseph Fortman <jdbf89@gmail.com>
2025-03-18 12:45:05 -0500

    updated docker compose; added sym link for /scripts

--- as-user.sh
+++ as-user.sh
@@ -16,6 +16,7 @@ fi
 mkdir -p ./"${GAME_DIR}"
 ln -s /addons         "./${GAME_DIR}/addons"
 ln -s /cfg            "./${GAME_DIR}/cfg"
+ln -s /scripts        "./${GAME_DIR}/scripts"
 ln -s /motd/host.txt  "./${GAME_DIR}/myhost.txt"
 ln -s /motd/motd.txt  "./${GAME_DIR}/mymotd.txt"
 
